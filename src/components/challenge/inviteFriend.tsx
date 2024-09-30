708f147fad8a662a2d6c73e619cbe07c8e95ac71
AniketSudke <aniketsudke20@gmail.com>
2024-10-01 00:09:18 +0530

    fronted-layout

--- src/components/challenge/inviteFriend.tsx
+++ src/components/challenge/inviteFriend.tsx
@@ -13,7 +13,6 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import exp from "constants"
 
 const  InviteFriend = () => {
   return (
